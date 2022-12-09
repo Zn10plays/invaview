5b71d79e5fc0898b575851a207cbe28387bfa4c2
Rudra Patel <rudrapatel26122004@gmail.com>
2022-12-08 21:14:38 -0500

    added firebase :)

---
+++ firebase/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxK38T_hd6fJjE-7IzW31YESofQy0gkdI",
+  authDomain: "invadev-368018.firebaseapp.com",
+  projectId: "invadev-368018",
+  storageBucket: "invadev-368018.appspot.com",
+  messagingSenderId: "745258602314",
+  appId: "1:745258602314:web:cc74cfc97820d7ce8f7bb3",
+  measurementId: "G-V0XCTWV53T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export {app, auth, analytics};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Paper from '@mui/material/Paper';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark'
+    }
+  })
+
+  return <ThemeProvider theme={darkTheme}>
+    <Paper sx={{height: '100vh'}} square>
+      <Component {...pageProps} />
+    </Paper>
+  </ThemeProvider>
 }
